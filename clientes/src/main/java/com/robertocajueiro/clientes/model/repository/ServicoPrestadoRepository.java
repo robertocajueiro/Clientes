e62f8dfe829daf85aada21ff3a3e6996626b6d53
robertocajueiro <beto.cajueiro@gmail.com>
2021-09-28 02:01:18 -0300

    Create ServicoPrestadoRepository.java

---
+++ clientes/src/main/java/com/robertocajueiro/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,9 @@
+package com.robertocajueiro.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.robertocajueiro.clientes.model.entity.ServicoPrestado;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+}
