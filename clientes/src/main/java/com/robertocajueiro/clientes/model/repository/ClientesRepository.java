b67318bbee11a48e34aca5b7b193e6e24ae3f781
robertocajueiro <beto.cajueiro@gmail.com>
2021-09-28 02:07:27 -0300

    Update ClientesRepository.java

--- clientes/src/main/java/com/robertocajueiro/clientes/model/repository/ClientesRepository.java
+++ clientes/src/main/java/com/robertocajueiro/clientes/model/repository/ClientesRepository.java
@@ -1,5 +1,9 @@
 package com.robertocajueiro.clientes.model.repository;
 
-public interface ClientesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.robertocajueiro.clientes.model.entity.Cliente;
+
+public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
 
 }
