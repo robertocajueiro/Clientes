8a1c9891ac9c9a0b6c70e4cba7921cacc630691a
Roberto Dantas Cajueiro <beto.cajueiro@gmail.com>
2021-09-28 12:39:02 -0300

    Acha por id Metodo ClienteController

--- clientes/src/main/java/com/robertocajueiro/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/robertocajueiro/clientes/rest/ClienteController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
 
@@ -28,15 +30,22 @@ public class ClienteController {
 		this.repository = repository;
 	}
 	
-	@GetMapping
-	public List<Cliente> obterTodos(){
-		return repository.findAll();
-	}
-	
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar( @RequestBody @Validated Cliente cliente ){
         return repository.save(cliente);
+    }
+    
+    @GetMapping
+	public List<Cliente> obterTodos(){
+		return repository.findAll();
+	}
+    
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id) {
+    	return repository
+    			.findById(id)
+    			.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
     }
 
 }
