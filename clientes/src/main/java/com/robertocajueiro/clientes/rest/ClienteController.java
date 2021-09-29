6ed35d01b26be7ca8706a08f93b176c2ddcbcf79
robertocajueiro <beto.cajueiro@gmail.com>
2021-09-29 18:29:49 -0300

    Update ClienteController.java
    Metodo Deletar

--- clientes/src/main/java/com/robertocajueiro/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/robertocajueiro/clientes/rest/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,18 @@ public class ClienteController {
     			.findById(id)
     			.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
     }
+    
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+    	 repository
+    	 .findById(id)
+    	 .map(cliente -> {
+    		repository.delete(cliente);
+    		return Void.TYPE;
+    	 })
+    	 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+    }
+    
 
 }
