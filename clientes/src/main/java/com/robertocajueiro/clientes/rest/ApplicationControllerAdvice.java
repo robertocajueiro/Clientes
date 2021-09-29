4d81936034094e1f93efe5e61333c2ff0c24986f
robertocajueiro <beto.cajueiro@gmail.com>
2021-09-29 20:48:26 -0300

    Update ApplicationControllerAdvice.java

--- clientes/src/main/java/com/robertocajueiro/clientes/rest/ApplicationControllerAdvice.java
+++ clientes/src/main/java/com/robertocajueiro/clientes/rest/ApplicationControllerAdvice.java
@@ -1,16 +1,17 @@
 package com.robertocajueiro.clientes.rest;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.robertocajueiro.clientes.rest.exception.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.robertocajueiro.clientes.rest.exception.ApiErrors;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -24,5 +25,13 @@ public class ApplicationControllerAdvice {
                 .map(objectError -> objectError.getDefaultMessage())
                 .collect(Collectors.toList());
         return new ApiErrors(messages);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ApiErrors> handleResponseStatusException(ResponseStatusException ex){
+        String mensagemErro = ex.getReason();
+        HttpStatus codigoStatus = ex.getStatus();
+        ApiErrors apiErrors = new ApiErrors(mensagemErro);
+        return new ResponseEntity<ApiErrors>(apiErrors, codigoStatus);
     }
 }
