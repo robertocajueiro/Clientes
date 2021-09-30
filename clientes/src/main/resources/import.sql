515554d60f5c66a601a14d92a52c82004569a3f2
robertocajueiro <beto.cajueiro@gmail.com>
2021-09-29 21:16:31 -0300

    Update import.sql

--- clientes/src/main/resources/import.sql
+++ clientes/src/main/resources/import.sql
@@ -1,5 +1,5 @@
---insert into cliente (nome, cpf) values ('Roberto Dantas', '12345678901');
---insert into cliente (nome, cpf) values ('Lucas Batista', '00012355512');
+-- insert into cliente (nome, cpf) values ('Roberto Dantas', '12345678901');
+-- insert into cliente (nome, cpf) values ('Lucas Batista', '00012355512');
 
---insert into servico_prestado (descricao, id_cliente, valor) values ('Manuntenção computador', 1, 120);
---insert into servico_prestado (descricao, id_cliente, valor) values ('Manuntenção computador', 2, 120);
+-- insert into servico_prestado (descricao, id_cliente, valor) values ('Manuntenção computador', 1, 120);
+-- insert into servico_prestado (descricao, id_cliente, valor) values ('Manuntenção computador', 2, 120);
