88676995ae8bdb6d9ee9b385bd4888ce6dbc9d91
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-12-02 15:43:16 -0300

    Criando o modulo e os componentes de servico prestado

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
 import { ClientesModule } from './clientes/clientes.module';
+import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module'
+
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { ClientesModule } from './clientes/clientes.module';
     BrowserModule,
     AppRoutingModule,
     TemplateModule,
-    ClientesModule
+    ClientesModule,
+    ServicoPrestadoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
